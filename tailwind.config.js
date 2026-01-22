135b8fa1948e83bcd5dbb686dbc9e2324a850b49
benoittonnot-afk <benoittonnot@googlemail.com>
2026-01-22 20:40:28 +0100

    [Blink] Restored to version: ver-w9xwdw06

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,6 @@ export default {
           foreground: 'hsl(var(--muted-foreground))',
         },
         border: 'hsl(var(--border))',
-        brand: {
-          frost: 'hsl(var(--milkyway-frost))',
-          noir: 'hsl(var(--lingonberry-noir))',
-          sky: 'hsl(var(--polar-sky))',
-          stillness: 'hsl(var(--stillness))',
-          sun: 'hsl(var(--arctic-sun))',
-        }
       },
       fontFamily: {
         serif: ['Playfair Display', 'serif'],
